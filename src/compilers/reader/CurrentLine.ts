aa748aff191f1df15a58aa24481f692fa0a746d2
Asaki Yuki <vantrong2007vn@gmail.com>
2025-02-18 11:19:34 +0700

    fixed path displaying as undefined when showing build log results on Linux

--- src/compilers/reader/CurrentLine.ts
+++ src/compilers/reader/CurrentLine.ts
@@ -1,4 +1,9 @@
+const regex: any = {
+    win32: /[A-Z]:(.+):\d+:\d+/g,
+    linux: /\/(.+):\d+:\d+/g,
+};
+
 export function CurrentLine() {
-	const lines = new Error("Code Line").stack?.match(/[A-Z]:(.+):\d+:\d+/g);
-	return lines?.[lines.length - 1];
+    const lines = new Error("Code Line").stack?.match(regex[process.platform]);
+    return lines ? lines[lines.length - 1] : "Unknown";
 }
